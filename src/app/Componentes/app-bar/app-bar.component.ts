7cac41f255d334a241377dbfaaa55f4d0207c738
Fernando Ramos Murillo <fernandoramosmurillo738@gmail.com>
2026-03-21 17:29:42 +0100

    Mejora del menu header de ambas versiones

--- src/app/Componentes/app-bar/app-bar.component.ts
+++ src/app/Componentes/app-bar/app-bar.component.ts
@@ -5,7 +5,7 @@ import { IonMenu, IonHeader, IonApp, IonToolbar, IonTitle, IonButtons, IonConten
   selector: 'app-bar',
   templateUrl: './app-bar.component.html',
   styleUrls: ['./app-bar.component.scss'],
-  imports: [],
+  imports: [IonHeader],
 })
 export class AppBarComponent  implements OnInit {
 
--- src/app/Componentes/app-frame/app-frame-desktop-component/app-frame-desktop-component.component.ts
+++ src/app/Componentes/app-frame/app-frame-desktop-component/app-frame-desktop-component.component.ts
@@ -1,11 +1,12 @@
 import { Component, computed, effect, inject, OnInit, signal, Signal } from '@angular/core';
 import { IonHeader, IonContent, IonRouterOutlet, IonImg } from "@ionic/angular/standalone";
+import { AppBarComponent } from "../../app-bar/app-bar.component";
 
 @Component({
   selector: 'app-frame-desktop-component',
   templateUrl: './app-frame-desktop-component.component.html',
   styleUrls: ['./app-frame-desktop-component.component.scss'],
-  imports: [IonHeader, IonContent, IonRouterOutlet],
+  imports: [IonHeader, IonContent, IonRouterOutlet, AppBarComponent],
 })
 export class AppFrameDesktopComponentComponent {
 }
