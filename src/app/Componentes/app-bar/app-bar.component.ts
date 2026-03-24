d9faa6efa1c8dd9d001fef253844a03891f2ae1f
Fernando Ramos Murillo <fernandoramosmurillo738@gmail.com>
2026-03-24 15:48:07 +0100

    Preparando el header de escritorio

--- src/app/Componentes/app-bar/app-bar.component.ts
+++ src/app/Componentes/app-bar/app-bar.component.ts
@@ -1,13 +1,13 @@
 import { environment } from '@env/environment';
 import { Component, OnInit } from '@angular/core';
-import { IonMenu, IonHeader, IonApp, IonToolbar, IonTitle, IonButtons, IonContent, IonMenuButton } from "@ionic/angular/standalone";
+import { IonMenu, IonHeader, IonApp, IonToolbar, IonTitle, IonButtons, IonContent, IonMenuButton, IonButton } from "@ionic/angular/standalone";
 
 
 @Component({
   selector: 'app-bar',
   templateUrl: './app-bar.component.html',
   styleUrls: ['./app-bar.component.scss'],
-  imports: [IonHeader],
+  imports: [IonButton, IonHeader],
 })
 export class AppBarComponent  implements OnInit {
 
--- src/app/Componentes/app-frame/app-frame-mobile-component/app-frame-mobile.component.ts
+++ src/app/Componentes/app-frame/app-frame-mobile-component/app-frame-mobile.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '@env/environment';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonMenu, IonRouterOutlet } from "@ionic/angular/standalone";
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonMenu, IonRouterOutlet, IonText } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-frame-mobile-component',
   templateUrl: './app-frame-mobile.component.html',
   styleUrls: ['./app-frame-mobile.component.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonMenu, IonRouterOutlet],
+  imports: [IonText, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonMenu, IonRouterOutlet],
 })
 export class AppFrameMobileComponent  implements OnInit {
 
