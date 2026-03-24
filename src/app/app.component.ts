f85f673b77e8ec0fe1350e5323e80f9edb7e5ca3
Fernando Ramos Murillo <fernandoramosmurillo738@gmail.com>
2026-03-24 16:43:52 +0100

    Implementacion del ion-toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { IonIcon } from '@ionic/angular/standalone';
 import { Component, inject } from '@angular/core';
 import { IonApp, IonRouterOutlet, IonHeader, IonContent, Platform } from '@ionic/angular/standalone';
 import { AppBarComponent } from "./Componentes/app-bar/app-bar.component";
 import { AppFrameComponent } from "./Componentes/app-frame/app-frame.component";
+import { addIcons } from 'ionicons';
+import { home} from 'ionicons/icons'
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonContent, IonHeader, IonApp, IonRouterOutlet, AppBarComponent, AppFrameComponent],
+  imports: [IonContent, IonHeader, IonApp, IonRouterOutlet, AppBarComponent, AppFrameComponent, IonIcon],
 })
 export class AppComponent {
-
+  constructor() {
+    addIcons({
+      home
+    });
+  }
 }
--- src/app/paginas/home/home.page.ts
+++ src/app/paginas/home/home.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonCol, IonGrid, IonRow, IonText, IonCardContent, IonCardHeader, IonCard, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonCol, IonGrid, IonRow, IonText, IonCardContent, IonCardHeader, IonCard, IonCardTitle, IonCardSubtitle, IonImg } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonCardSubtitle, IonCardTitle, IonCard, IonCardHeader, IonCardContent, IonText, IonRow, IonGrid, IonCol, IonContent, IonHeader, CommonModule, FormsModule]
+  imports: [IonImg, IonCardSubtitle, IonCardTitle, IonCard, IonCardHeader, IonCardContent, IonText, IonRow, IonGrid, IonCol, IonContent, IonHeader, CommonModule, FormsModule]
 })
 export class HomePage implements OnInit {
 
