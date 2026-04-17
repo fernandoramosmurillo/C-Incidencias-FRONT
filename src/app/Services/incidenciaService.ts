9da3afcb1c60d7b7872622fbd587ea5a821edd8d
Fernando Ramos Murillo <fernandoramosmurillo738@gmail.com>
2026-04-17 08:17:28 +0200

    Se ha creado el servicio Incidencia

---
+++ src/app/Services/incidenciaService.ts
@@ -0,0 +1,32 @@
+import { Subscription } from 'rxjs';
+import { Incidencia } from '../Interfaces/incidencia';
+import { HttpService } from './http-service';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IncidenciaService {
+  private suscripcionIncidencias?: Subscription;
+  HttpService: HttpService = inject(HttpService);
+
+  incidencias: Incidencia[] = [];
+
+  cargarDatos(): Subscription {
+    return (this.suscripcionIncidencias =
+      this.HttpService.obtenerDatos<Incidencia>('incidencias').subscribe(
+        (data: Incidencia[]) => {
+          this.incidencias = data;
+          console.log('Datos cargados con éxito');
+        },
+      ));
+  }
+
+  // Cuando el componente se destruye, cerramos la suscripción
+  cerrarSuscripcion(): void {
+    if (this.suscripcionIncidencias) {
+      this.suscripcionIncidencias.unsubscribe();
+      console.log('Suscripción cerrada manualmente');
+    }
+  }
+}
