7913ac0e99b71fba0aa9f076c5476a646b0d7e09
Fernando Ramos Murillo <fernandoramosmurillo738@gmail.com>
2026-04-20 21:28:50 +0200

    Funcion vincularPropiedad realizada

--- src/app/Services/BaseService.ts
+++ src/app/Services/BaseService.ts
@@ -25,4 +25,32 @@ export abstract class BaseService<T> {
       return referencia.split('/').pop() || '';
     return referencia?.idUsuario || '';
   }
+
+  protected vincularPropiedad<T, R>(
+    lista: T[],
+    propiedad: keyof T,
+    mapa: Map<string, R>,
+  ): T[] {
+    return lista.map((item) => {
+      // 1. Obtenemos lo que hay ahora (el path de Java o el objeto parcial)
+      const referencia = item[propiedad];
+
+      // 2. Extraemos el ID limpio (usando tu método extraerId)
+      const idLimpio = this.extraerId(referencia);
+
+      // 3. Buscamos en el mapa que pasamos por parámetro
+      const objetoVinculado = mapa.get(idLimpio);
+
+      // 4. Si lo encontramos, inyectamos el objeto completo
+      if (objetoVinculado) {
+        return {
+          ...item,
+          [propiedad]: objetoVinculado,
+        };
+      }
+
+      // Si no, devolvemos el item original
+      return item;
+    });
+  }
 }
--- src/app/Services/incidenciaService.ts
+++ src/app/Services/incidenciaService.ts
@@ -3,7 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { UsuarioService } from './usuarioService';
 import { Vinculable } from '../Interfaces/vinculable';
 import { BaseService } from './BaseService';
-import { Ciudadano } from '../Interfaces/ciudadano';
+import { Usuario } from '../Interfaces/usuario';
 
 @Injectable({
   providedIn: 'root',
@@ -19,36 +19,9 @@ export class IncidenciaService
   asignarModelos() {
     this.datos.update((lista) => {
 
-      let nuevaLista = this.vincularCiudadanos(lista);
+      let nuevaLista = this.vincularPropiedad<Incidencia, Usuario>(lista, 'usuarioCiudadano', new Map(this.usuarioService.datos().map((u) => [u.idUsuario, u])));
 
       return nuevaLista;
-    });
-  }
-
-  vincularCiudadanos(lista: Incidencia[]): Incidencia[] {
-
-    const listaCiudadanos: Ciudadano[] = this.usuarioService.filtrarCiudadanos(
-      this.usuarioService.datos(),
-    ) as Ciudadano[];
-
-    const usuarioMap = new Map(
-      listaCiudadanos.map((usuario) => [usuario.idUsuario, usuario]),
-    );
-
-    return lista.map((incidencia) => {
-      const referenciaUsuarioCiudadano: string =
-        incidencia.usuarioCiudadano as any;
-      const ciudadano = usuarioMap.get(
-        this.extraerId(referenciaUsuarioCiudadano),
-      );
-
-      if (ciudadano) {
-        return {
-          ...incidencia,
-          usuarioCiudadano: ciudadano as Ciudadano,
-        };
-      }
-      return incidencia;
     });
   }
 }
