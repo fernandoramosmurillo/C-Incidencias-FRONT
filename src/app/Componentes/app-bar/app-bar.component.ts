8eaaef0ae82ee3472366475a27551000b557922a
Fernando Ramos Murillo <fernandoramosmurillo738@gmail.com>
2026-04-21 08:54:01 +0200

    Aplicando el avatar en app-bar

--- src/app/Componentes/app-bar/app-bar.component.ts
+++ src/app/Componentes/app-bar/app-bar.component.ts
@@ -1,13 +1,13 @@
 import { environment } from '@env/environment';
 import { Component, OnInit } from '@angular/core';
-import { IonMenu, IonHeader, IonApp, IonToolbar, IonTitle, IonButtons, IonContent, IonMenuButton, IonButton, IonIcon } from "@ionic/angular/standalone";
+import { IonMenu, IonHeader, IonApp, IonToolbar, IonTitle, IonButtons, IonContent, IonMenuButton, IonButton, IonIcon, IonAvatar } from "@ionic/angular/standalone";
 
 
 @Component({
   selector: 'app-bar',
   templateUrl: './app-bar.component.html',
   styleUrls: ['./app-bar.component.scss'],
-  imports: [IonIcon, IonButton, IonHeader, IonToolbar, IonButtons],
+  imports: [IonAvatar, IonIcon, IonButton, IonHeader, IonToolbar, IonButtons],
 })
 export class AppBarComponent  implements OnInit {
 
