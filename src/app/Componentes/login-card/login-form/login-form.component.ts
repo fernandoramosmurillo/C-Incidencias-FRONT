4ba3cda64c05ac7b352f6eb6d00bac716e24ead7
Fernando Ramos Murillo <fernandoramosmurillo738@gmail.com>
2026-04-25 15:22:12 +0200

    Añadiendo inputs del formulario

--- src/app/Componentes/login-card/login-form/login-form.component.ts
+++ src/app/Componentes/login-card/login-form/login-form.component.ts
@@ -1,14 +1,35 @@
+import { CapacitorHttp } from '@capacitor/core';
+import { Ciudadano } from './../../../Interfaces/ciudadano';
 import { Component, OnInit } from '@angular/core';
+import { IonInput, IonGrid, IonItem, IonLabel, IonCol, IonRow, IonList, IonText, IonButton } from "@ionic/angular/standalone";
+import { RolesUsuario, TiposAcceso, Usuario } from 'src/app/Interfaces/usuario';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
+  imports: [IonButton, IonText, IonList, IonRow, IonCol, IonLabel, IonItem, IonGrid, IonInput, FormsModule],
 })
-export class LoginFormComponent  implements OnInit {
+export class LoginFormComponent {
 
-  constructor() { }
+  usuario: Usuario = {
+    estado: '',
+    idUsuario: '',
+    nombre: '',
+    apellidos: '',
+    correoElectronico: '',
+    clave: '',
+    fechaNacimiento: null,
+    fechaCreacion: null,
+    fechaEliminacion: null,
+    fotoPerfilUrl: null,
+    bloqueado: false,
+    recibirNotificaciones: false,
+    notificacionesRecibidas: [],
+    rolUsuario: RolesUsuario.CIUDADANO,
+    tipoAcceso: TiposAcceso.CORREO_CONTRASEÑA
+  };
 
-  ngOnInit() {}
 
 }
