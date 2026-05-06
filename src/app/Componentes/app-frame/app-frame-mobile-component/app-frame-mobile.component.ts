1015a1c16b71125693515f2ec8812878437b01ca
Fernando Ramos Murillo <fernandoramosmurillo738@gmail.com>
2026-05-06 16:56:22 +0200

    Avatar button arreglado

--- src/app/Componentes/app-frame/app-frame-mobile-component/app-frame-mobile.component.ts
+++ src/app/Componentes/app-frame/app-frame-mobile-component/app-frame-mobile.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '@env/environment';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonMenu, IonRouterOutlet, IonText } from "@ionic/angular/standalone";
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonMenu, IonRouterOutlet, IonText, IonButton } from "@ionic/angular/standalone";
+import { AvatarButtonComponent } from "../../avatar-button/avatar-button.component";
 
 @Component({
   selector: 'app-frame-mobile-component',
   templateUrl: './app-frame-mobile.component.html',
   styleUrls: ['./app-frame-mobile.component.scss'],
-  imports: [IonText, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonMenu, IonRouterOutlet],
+  imports: [IonButton, IonText, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonMenu, IonRouterOutlet, AvatarButtonComponent],
 })
 export class AppFrameMobileComponent  implements OnInit {
 
