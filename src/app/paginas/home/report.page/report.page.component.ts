6a9360273c452821b6d30ec0c6e319b00d869784
Fernando Ramos Murillo <fernandoramosmurillo738@gmail.com>
2026-05-09 12:11:34 +0200

    Report y home mejorados

--- src/app/paginas/home/report.page/report.page.component.ts
+++ src/app/paginas/home/report.page/report.page.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { IonContent, IonCardHeader, IonCard, IonCardTitle, IonCardSubtitle, IonCardContent, IonLabel, IonItem, IonInput, IonSelect, IonSelectOption, IonButton, IonIcon, IonTextarea, IonList } from "@ionic/angular/standalone";
+import { IonContent, IonCardHeader, IonCard, IonCardTitle, IonCardSubtitle, IonCardContent, IonLabel, IonItem, IonInput, IonSelect, IonSelectOption, IonButton, IonIcon, IonTextarea, IonList, IonGrid, IonRow, IonCol, IonImg } from "@ionic/angular/standalone";
 import { ReportFormComponent } from "src/app/Componentes/report-form/report-form.component";
 
 @Component({
   selector: 'report.page',
   templateUrl: './report.page.component.html',
-  imports: [IonList, IonTextarea, IonIcon, IonButton, IonInput, IonItem, IonLabel, IonCardContent, IonCardSubtitle, IonCardTitle, IonContent, IonCardHeader, IonCard, IonSelect, IonSelectOption, ReportFormComponent],
+  imports: [IonImg, IonCol, IonRow, IonGrid, IonList, IonTextarea, IonIcon, IonButton, IonInput, IonItem, IonLabel, IonCardContent, IonCardSubtitle, IonCardTitle, IonContent, IonCardHeader, IonCard, IonSelect, IonSelectOption, ReportFormComponent],
   styleUrls: ['./report.page.component.scss'],
 })
 export class ReportPageComponent  implements OnInit {
